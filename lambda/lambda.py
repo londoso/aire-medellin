cbd91720aa739b112094d9e35e8f10bcf76c8057
Anderson Londoño Osorio <londoso@gmail.com>
2024-01-17 01:07:17 -0500

    lambda lista

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -1,63 +1,37 @@
-import json
-import boto3
-import urllib.parse
-import urllib3
 import os
 import awswrangler as wr
-from dateutil import parser
-    
-def get_s3_object(bucket,key):
-    client = boto3.client('s3')
-    response = client.get_object(Bucket=bucket, Key=key)
-    return response
+from datetime import datetime, timedelta
 
-def notify(sub, msg, sns_arn):
-    print('Notificando errores por SNS')
-    sns = boto3.client('sns')
-    sns.publish(
-        TopicArn = sns_arn,
-        Message = msg,
-        Subject = sub
-    )
+def fechas_unicas(lista):
+    l_replace = [lst.replace(bucket_name_prefix, '') for lst in lista]
+    dates = ['-'.join(lst.split('-')[:3]) for lst in l_replace]
+    unique_dates = set(dates)
+    return unique_dates
 
 def lambda_handler(event, context):
 
-    batch_size = int(os.environ['BATCH_SIZE'])
-    table = os.environ['DYNAMODB_TABLE']
-    bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
-    eventTime = event['Records'][0]['eventTime']
-    event_time_file = parser.isoparse(eventTime)
+    bucket_name = os.environ['BucketOrigen']
+    bucket_name_prefix = f's3://{bucket_name}/' 
+    bucket_destino = os.environ['BucketDestino']
+    fecha_col = (datetime.now() - timedelta(hours=5)).strftime('%Y-%m-%d')
     
     try:
-        data = wr.s3.read_csv(f"s3://{bucket}/{key}",chunksize=batch_size,dtype=object)
-        print('eventTime: ',eventTime)
-        
-        for x in data:
-            print('----parte inicia------')
-            print(x['user_id'].astype(str).to_list())
-            subjectIds = x['user_id'].astype(str).to_list()
-            regulateId = request_api(subjectIds)
-            save_to_dynamodb(table, regulateId, subjectIds)
-            
-            print('----parte fin------')
-            
-        replace_filenames = {key.replace('in/', ''):f"{event_time_file.strftime('%Y%m%d%H%M%S')}.csv"}
+        lista_objetos = wr.s3.list_objects(bucket_name_prefix)
+        fechas = fechas_unicas(lista_objetos)
 
-        wr.s3.copy_objects(
-            paths=[f"s3://{bucket}/{key}"],
-            source_path=f"s3://{bucket}/in/",
-            target_path=f"s3://{bucket}/processed/",
-            replace_filenames=replace_filenames
-        )
-        
-        wr.s3.delete_objects([f"s3://{bucket}/{key}"])
-        
-        return 'Procesado: ' + key
+        for fecha in fechas:
+            if fecha != fecha_col:
+                fc = fecha.split('-')
+                path_origen = f's3://{bucket_name}/{fecha}*'
+                path_destino = f's3://{bucket_destino}/{fc[0]}/{fc[1]}/{fc[2]}/consolidado.csv'
+                objetos = [i for i in lista_objetos if fecha in i]
+                full = wr.s3.read_csv(path_origen)    
+                wr.s3.to_csv(df=full, path=path_destino, index=False)
+                print('## Guardado:', path_destino)
+                wr.s3.delete_objects(path_origen)
+                print('## Eliminando objetos:')
+                for eliminar in objetos:
+                    print('-', eliminar)
         
     except Exception as e:
         print(e)
-        message = str(e) + '. Please check Cloudwatch Logs.'
-        subject = 'Error Lambda [segment_suppress_dev]'
-        sns = os.environ['ARN_SNS_ERROR']
-        notify(subject, message, sns)
